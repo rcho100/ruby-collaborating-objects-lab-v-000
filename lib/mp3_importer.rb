26a7dade1af6b6decbfb2b2b9c53df5c5fc4e23a
Richard Cho <richardcho100@gmail.com>
2018-05-17 20:47:03 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -11,6 +11,11 @@ class MP3Importer
   end
 
   def import
-    self.files.collect {|file| Song.new_by_filename(file)}
+    self.files.collect do |file|
+      song = Song.new_by_filename(file)
+      Artist.all << song.artist
+    end
+
+    self.files
   end
 end
