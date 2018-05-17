6dc4ba075de25d2a10ae89e74110699c9e613e30
Richard Cho <richardcho100@gmail.com>
2018-05-17 19:57:21 +0000

    automatically backed up by learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,16 @@
+require "pry"
+class MP3Importer
+  attr_accessor :path
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    @files = Dir.entries(@path)
+    @files.delete_if {|file| file == "." || file == ".."}
+  end
+
+  def import
+
+  end
+end
